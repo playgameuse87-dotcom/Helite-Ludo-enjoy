668f89c3cdfcaa757a08264005f1b3e0301df510
playgameuse87-dotcom <playgameuse87@gmail.com>
2025-11-17 14:16:27 +0100

    Index.tsx

---
+++ src/pages/Index.tsx
@@ -0,0 +1,11 @@
+import { LudoGame3D } from "@/components/ludo3d/LudoGame3D";
+
+const Index = () => {
+  return (
+    <div className="min-h-screen bg-background">
+      <LudoGame3D />
+    </div>
+  );
+};
+
+export default Index;
